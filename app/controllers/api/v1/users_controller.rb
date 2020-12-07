8ceedac2b75f2850bdc5cc54a4092350bbaebc20
Rick <rickmaxg3@hotmail.com>
2020-12-07 12:37:29 -0300

    update users index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate_api_v1_user!
+  before_action :set_users
 
   def index
-    "passou!!!"
+    render_success(200, nil, { data: format_users(@users) } )
   end
+
+  private
+
+    def set_users
+      @users = current_api_v1_user.admin? ? User.all : [current_api_v1_user]
+    end
+    
+    def format_users(users)
+      resp = users.map { |user| user.attributes.slice( 'first_name', 'last_name', 'email' ) }
+    end
 end
